029647b15eae0c870aa60da021ab3daff52e0378
Larissasbueno <larissasbuenno@gmail.com>
2023-03-16 07:45:41 -0300

    add mod9

---
+++ mod9/Cliente.java
@@ -0,0 +1,28 @@
+package br.com.lbueno;
+
+import java.awt.image.ShortLookupTable;
+
+/**
+ * @author lbueno
+ */
+
+public class Cliente {
+
+
+    private Integer codigo;
+    // ou private Long codigo;
+
+    private String nome;
+    private Long codigoMaior = 12542452l;
+
+    private Double valorDecimal = (double) 10.1;
+
+    private Boolean status = false;
+
+    private Short shor;
+
+    private Byte bi;
+
+    private Character letra = 'A';
+
+}
---
+++ mod9/Exemplo1.java
@@ -0,0 +1,33 @@
+package br.com.lbueno;
+
+/**
+ * @author lbueno
+ */
+
+public class Exemplo1 {
+
+    public static void main(String args[]){
+
+        //boxing
+        Boolean status = true;
+        Boolean status1 = Boolean.TRUE;
+
+        Character c = 'A';
+
+        Integer idade = 10;
+
+        Long cpf = Long.valueOf(455354);
+        Long cpf1 = 23213545132l;
+
+
+        //unboxing
+        boolean status2 = Boolean.TRUE;
+        char letra = Character.valueOf('A');
+
+        int idade2 = Integer.valueOf(12);
+
+
+
+
+    }
+}
