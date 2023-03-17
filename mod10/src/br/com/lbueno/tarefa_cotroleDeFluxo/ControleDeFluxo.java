fca4364f2741ce9646b0c218387d4c45b84433ae
Larissa S. Bueno <117838683+Larissasbueno@users.noreply.github.com>
2023-03-17 17:24:51 -0300

    commit correcao

--- mod10/src/br/com/lbueno/tarefa_cotroleDeFluxo/ControleDeFluxo.java
+++ mod10/src/br/com/lbueno/tarefa_cotroleDeFluxo/ControleDeFluxo.java
@@ -2,18 +2,18 @@ package br.com.lbueno.tarefa_cotroleDeFluxo;
 
 public class ControleDeFluxo {
     public static void main (String args[]) {
-        int n1 = 0;
-        int n2 = 5;
-        int n3 = 5;
-        int n4 = 5;
+        double n1 = 5.5;
+        double n2 = 9;
+        double n3 = 4.5;
+        double n4 = 10;
 
-        int media = (n1 + n2 + n3 + n4) / 4;
+        double media = (n1 + n2 + n3 + n4) / 4;
         System.out.println(media);
 
-        int result = media;
+        double result = media;
          if (result >= 7) {
             System.out.println("Aprovado");
-        } else if  (result == 5){
+        } else if  (result >= 5){
             System.out.println("Recuperação");
         } else if (result <5){
             System.out.println("Reprovado");
